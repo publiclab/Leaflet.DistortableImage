7e05977ba32ab124a8181c223e8ef2373a98b5d7
Anish Shah <anishshah101@gmail.com>
2014-07-25 06:29:57 +0530

    Adding the extended ImageMarker class and image

---
+++ imagedistort/imagemarker.js
@@ -0,0 +1,15 @@
+L.ImageMarker = L.Marker.extend({
+				options: {
+			pane: 'markerPane',
+			icon: new L.Icon({iconUrl:'ImageDistort/images/imarker.png'}),
+			// title: '',
+			// alt: '',
+			clickable: true,
+			draggable: true,
+			keyboard: true,
+			zIndexOffset: 0,
+			opacity: 1,
+			riseOnHover: true,
+			riseOffset: 250
+			}
+		});
