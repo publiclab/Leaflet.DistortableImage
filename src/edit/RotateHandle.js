10a5271d657195b27c1ac0cbbc35f6384f7bfffe
Justin Manley <manleyjster@gmail.com>
2014-12-15 00:53:23 -0600

    Add rotation and scaling interactions to RotateHandle. Implement RotateHandle#_calculateAngle using the law of cosines.

--- src/edit/RotateHandle.js
+++ src/edit/RotateHandle.js
@@ -1,3 +1,69 @@
 L.RotateHandle = L.EditHandle.extend({
-	
+	options: {
+		icon: new L.Icon({ 
+			iconUrl: '../src/images/circle-o_cc4444_16.png',
+			iconSize: [16, 16],
+			iconAnchor: [8, 8]}
+		)
+	},
+
+	_onHandleDrag: function() {
+		var overlay = this._handled,
+			formerLatLng = this._handled._corners[this._corner],
+			newLatLng = this.getLatLng(),
+
+			angle = this._calculateAngle(formerLatLng, newLatLng),
+			scale = this._calculateScalingFactor(formerLatLng, newLatLng);
+
+		overlay.editing._rotateBy(angle);
+		overlay.editing._scaleBy(scale);
+
+		overlay.fire('update');
+	},
+
+	updateHandle: function() {
+		this.setLatLng(this._handled._corners[this._corner]);
+	},
+
+	/* Takes two latlngs and calculates the angle between them. */
+	_calculateAngle: function(latlngA, latlngB) {
+		var map = this._handled._map,
+
+			centerPoint = map.latLngToLayerPoint(this._handled.getCenter()),
+			formerPoint = map.latLngToLayerPoint(latlngA),
+			newPoint = map.latLngToLayerPoint(latlngB),
+
+			formerRadiusSquared = this._d2(centerPoint, formerPoint),
+			newRadiusSquared = this._d2(centerPoint, newPoint);
+
+		/* Calculate the angle between the two points using the Law of Cosines. */
+		return Math.acos(
+			(newRadiusSquared + 
+				formerRadiusSquared - 
+				this._d2(newPoint, formerPoint)) /
+			(2*Math.sqrt(formerRadiusSquared)*Math.sqrt(newRadiusSquared))
+		);
+	},
+
+	/* Takes two latlngs and calculates the scaling difference. */
+	_calculateScalingFactor: function(latlngA, latlngB) {
+		var map = this._handled._map,
+
+			centerPoint = map.latLngToLayerPoint(this._handled.getCenter()),
+			formerPoint = map.latLngToLayerPoint(latlngA),
+			newPoint = map.latLngToLayerPoint(latlngB),
+
+			formerRadiusSquared = this._d2(centerPoint, formerPoint),
+			newRadiusSquared = this._d2(centerPoint, newPoint);
+
+		return Math.sqrt(newRadiusSquared / formerRadiusSquared);
+	},
+
+	/* Distance between two points in cartesian space, squared (distance formula). */
+	_d2: function(a, b) {
+		var dx = a.x - b.x,
+			dy = a.y - b.y;
+
+		return Math.pow(dx, 2) + Math.pow(dy, 2);
+	}
 });
