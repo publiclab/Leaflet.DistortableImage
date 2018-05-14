052cb8db2d9d9fa208051cdb473b6f4bcd5bea3e
John905 <antocles@comcast.net>
2018-05-14 11:58:44 -0600

    Added references to new node_modules needed for exporting image

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,88 +4,91 @@
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+	// base path that will be used to resolve all patterns (eg. files, exclude)
+	basePath: '../',
 
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+	// frameworks to use
+	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	frameworks: ['mocha'],
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: 'examples/*.jpg', included: false, served: true },
-      'node_modules/leaflet/dist/leaflet-src.js',
-      'node_modules/leaflet/dist/leaflet.css',
-      'node_modules/leaflet-toolbar/dist/leaflet.toolbar.js',
-      'node_modules/leaflet-toolbar/dist/leaflet.toolbar.css',
-      'node_modules/chai/chai.js',
-      'node_modules/sinon/pkg/sinon.js',
-      'src/util/*.js',
-      'src/edit/EditHandle.js',
-      'src/edit/LockHandle.js',
-      'src/edit/DistortHandle.js',
-      'src/edit/RotateHandle.js',
-      'src/DistortableImageOverlay.js',
-      'src/edit/DistortableImage.EditToolbar.js',      
-      'src/edit/DistortableImage.Edit.js',
-      'test/SpecHelper.js',
-      'test/src/**/*Spec.js'
-    ],
+	// list of files / patterns to load in the browser
+	files: [
+		{ pattern: 'examples/*.jpg', included: false, served: true },
+		'node_modules/leaflet/dist/leaflet-src.js',
+		'node_modules/leaflet/dist/leaflet.css',
+		'node_modules/leaflet-toolbar/dist/leaflet.toolbar.js',
+		'node_modules/leaflet-toolbar/dist/leaflet.toolbar.css',
+		'node_modules/chai/chai.js',
+		'node_modules/sinon/pkg/sinon.js',
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/webgl-distort/dist/webgl-distort.js',
+		'node_modules/glfx-js/dist/glfx.js',
+		'src/util/*.js',
+		'src/edit/EditHandle.js',
+		'src/edit/LockHandle.js',
+		'src/edit/DistortHandle.js',
+		'src/edit/RotateHandle.js',
+		'src/DistortableImageOverlay.js',
+		'src/edit/DistortableImage.EditToolbar.js',      
+		'src/edit/DistortableImage.Edit.js',
+		'test/SpecHelper.js',
+		'test/src/**/*Spec.js'
+	],
 
 
-    // so that karma can serve examples/example.jpg
-    proxies: {
-      '/examples/': '/base/examples/'
-    },
+	// so that karma can serve examples/example.jpg
+	proxies: {
+	  '/examples/': '/base/examples/'
+	},
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'mocha', 'coverage' ],
+	// test results reporter to use
+	// possible values: 'dots', 'progress'
+	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	reporters: [ 'mocha', 'coverage' ],
 
-    preprocessors: {
-      '../src/**/*.js': 'coverage'
-    },
+	preprocessors: {
+	  '../src/**/*.js': 'coverage'
+	},
 
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	// web server port
+	port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'PhantomJS' ],
+	// level of logging
+	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
 
-    plugins: [
-      'karma-mocha',
-      'karma-phantomjs-launcher',
-      'karma-mocha-reporter',
-      'karma-coverage'      
-    ],
+	// start these browsers
+	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	browsers: [ 'PhantomJS' ],
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+	plugins: [
+	  'karma-mocha',
+	  'karma-phantomjs-launcher',
+	  'karma-mocha-reporter',
+	  'karma-coverage'      
+	],
 
-    coverageReporter: {
-      reporters: [
-        { type: 'text', dir: '../coverage/', file: 'coverage.txt' },
-        { type: 'lcovonly', dir: '../coverage/' },
-        { type: 'html', dir: '../coverage/' }
-      ]
-    }    
-    
+
+	// Continuous Integration mode
+	// if true, Karma captures browsers, runs the tests and exits
+	singleRun: false,
+
+	coverageReporter: {
+	  reporters: [
+		{ type: 'text', dir: '../coverage/', file: 'coverage.txt' },
+		{ type: 'lcovonly', dir: '../coverage/' },
+		{ type: 'html', dir: '../coverage/' }
+	  ]
+	}    
+	
   });
 };
