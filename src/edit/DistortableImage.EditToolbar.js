ce4de7d5519ee5e6d88b29d97ed848bc45eaad2c
rexagod <rexagod@gmail.com>
2019-02-22 06:46:54 +0530

    Control toolbar + Keymap guide + Util fn to make custom toolbars

--- src/edit/DistortableImage.EditToolbar.js
+++ src/edit/DistortableImage.EditToolbar.js
@@ -10,10 +10,10 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 	}),
 
 	ToggleTransparency = EditOverlayAction.extend({
-		options: { toolbarIcon: { 
+		options: { toolbarIcon: {
 			html: '<span class="fa fa-adjust"></span>',
 			tooltip: 'Toggle Image Transparency',
-			title: 'Toggle Image Transparency'	
+			title: 'Toggle Image Transparency'
 		}},
 
 		addHooks: function() {
@@ -25,7 +25,7 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 	}),
 
 	ToggleOutline = EditOverlayAction.extend({
-		options: { toolbarIcon: { 
+		options: { toolbarIcon: {
 			html: '<span class="fa fa-square-o"></span>',
 			tooltip: 'Toggle Image Outline',
 			title: 'Toggle Image Outline'
@@ -40,7 +40,7 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 	}),
 
 	RemoveOverlay = EditOverlayAction.extend({
-		options: { toolbarIcon: { 
+		options: { toolbarIcon: {
 			html: '<span class="fa fa-trash"></span>',
 			tooltip: 'Delete image',
 			title: 'Delete image'
@@ -59,7 +59,7 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 		options: { toolbarIcon: {
 			html: '<span class="fa fa-lock"></span>',
 			tooltip: 'Lock / Unlock editing',
-			title: 'Lock / Unlock editing'			
+			title: 'Lock / Unlock editing'
 		}},
 
 		addHooks: function() {
@@ -78,7 +78,7 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 			options.toolbarIcon = {
 				html: '<span class="fa fa-' + icon + '"></span>',
 				tooltip: 'Rotate',
-				title: 'Rotate'	
+				title: 'Rotate'
 			};
 
 			EditOverlayAction.prototype.initialize.call(this, map, overlay, options);
@@ -101,18 +101,19 @@ var EditOverlayAction = LeafletToolbar.ToolbarAction.extend({
 				title: 'Export Image'
 			}
 		},
-		
+
 		addHooks: function ()
 		{
 			var editing = this._overlay.editing;
 
 			editing._toggleExport();
-			this.disable(); 
+			this.disable();
 		}
 	});
 
-L.DistortableImage.EditToolbar = LeafletToolbar.Popup.extend({
+L.DistortableImage.EditToolbar = LeafletToolbar.Control.extend({
 	options: {
+		position:'topleft',
 		actions: [
 			ToggleTransparency,
 			RemoveOverlay,
