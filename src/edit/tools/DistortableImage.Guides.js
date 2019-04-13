94e380395f222b4a0c952cbed5bc974dad351cdf
Sasha Boginsky <41092741+sashadev-sky@users.noreply.github.com>
2019-04-13 12:30:43 +0530

    Update src/edit/tools/DistortableImage.Guides.js
    Co-Authored-By: rexagod <rexagod@gmail.com>

--- src/edit/tools/DistortableImage.Guides.js
+++ src/edit/tools/DistortableImage.Guides.js
@@ -1,7 +1,11 @@
 L.DistortableImage = L.DistortableImage || {};
 
 // keymapper
-var dom_string = "<b><center><h3>Keymappings</h3><center><hr/></center></center><ul><li>L: Lock overlay</li><li>O: Outline overlay</li><li>R: Rotate overlay</li><li>RR: Distort overlay</li><li>T: Transparent overlay&nbsp;&nbsp;&nbsp;&nbsp;</li><li>DEL: Remove overlay</li></ul></b>";
+var dom_string = 
+"<b><center><h3>Keymappings</h3><center><hr/></center></center><ul>" + 
+"<li>L: Lock overlay</li>" + "<li>O: Outline overlay</li>" + "<li>R: Rotate overlay</li>" +
+"<li>RR: Distort overlay</li>" + "<li>T: Transparent overlay&nbsp;&nbsp;&nbsp;&nbsp;</li>" + 
+"<li>DEL: Remove overlay</li></ul></b>";
 
 var guide_strings = [dom_string]; // add guides here for custom toolbars
 
