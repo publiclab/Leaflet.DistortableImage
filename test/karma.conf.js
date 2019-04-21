7bc89ed6d8f976c7351c98c9d3ec35322aa4465f
rexagod <rexagod@gmail.com>
2019-04-21 14:48:39 +0530

    fix exports

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,10 +19,9 @@ module.exports = function(config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ["mocha"],
-    
+
     // list of files / patterns to load in the browser
     files: [
-      { pattern: "examples/*.jpg", included: false, served: true },
       { pattern: "examples/*.png", included: false, served: true },
       "node_modules/leaflet/dist/leaflet-src.js",
       "node_modules/leaflet/dist/leaflet.css",
