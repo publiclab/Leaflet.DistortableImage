695990f37ccdac9acab6e25f911a2335ad378eea
rexagod <rexagod@gmail.com>
2019-04-23 12:58:17 +0530

    standardize

--- src/edit/DistortableImage.Edit.js
+++ src/edit/DistortableImage.Edit.js
@@ -43,15 +43,14 @@ L.DistortableImage.Edit = L.Handler.extend({
         var el_wrapper = L.DomUtil.create("div", "l-container");
         el_wrapper.innerHTML = "<table><tbody>" +
         "<tr><th>Keymappings</th></tr>" +
-        "<tr><td><b>J/K: Order overlay</b></td></tr>" +
-        "<tr><td><b>L: Lock overlay</b></td></tr>" +
-        "<tr><td><b>O: Outline overlay</b></td></tr>" +
         "<tr><td><b>S: Scale Overlay</b></td></tr>" +
+        "<tr><td><b>O: Outline overlay</b></td></tr>" +
+        "<tr><td><b>L: Lock overlay</b></td></tr>" +
+        "<tr><td><b>J/K: Toggle order</b></td></tr>" +
         "<tr><td><b>T: Toggle transparency</b></td></tr>" +
-        "<tr><td><b>RR: Distort overlay</b></td></tr>" +
-        "<tr><td><b>ESC: Deselect overlay</b></td></tr>" +
+        "<tr><td><b>R: Toggle rotate/distort</b></td></tr>" +
+        "<tr><td><b>ESC: Remove handles</b></td></tr>" +
         "<tr><td><b>DEL: Remove overlay</b></td></tr>" +
-        "<tr><td><b>CAPS: Rotate overlay</b></td></tr>" +
         "</tbody></table>";
         return el_wrapper;
       };
