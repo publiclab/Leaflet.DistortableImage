6a56913ccaf1e9f8600a35c377f69bf0e214210b
sashadev-sky <sboginsky17@gmail.com>
2019-06-08 21:37:35 -0400

    reset b

--- src/edit/DistortableImage.Edit.js
+++ src/edit/DistortableImage.Edit.js
@@ -66,6 +66,12 @@ L.DistortableImage.Edit = L.Handler.extend({
     L.DomEvent.on(map, "click", this._deselect, this);
     L.DomEvent.on(overlay._image, "click", this._select, this);
 
+     /* Enable longpress */
+    L.DomEvent.on(overlay._image, 'contextmenu', function (e) { 
+      e.preventDefault();
+      if (this._mode === "lock") { this._toggleLock(); }
+    }, this);
+
     /* Enable hotkeys. */
     L.DomEvent.on(window, "keydown", this._onKeyDown, this);
   },
@@ -86,6 +92,12 @@ L.DistortableImage.Edit = L.Handler.extend({
     if (this.editing) { this.editing.disable(); }
 
     map.removeLayer(this._handles[this._mode]);
+
+     /* Enable longpress */
+    L.DomEvent.off(overlay._image, 'contextmenu', function (e) { 
+      e.preventDefault();
+      if (this._mode === "lock") { this._toggleLock(); }
+    }, this);
 
     /* Disable hotkeys. */
     L.DomEvent.off(window, "keydown", this._onKeyDown, this);
