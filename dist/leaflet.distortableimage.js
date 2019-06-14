4fa71a5125a677e33f2bbfb33f07d51a13b18a8d
sashadev-sky <sboginsky17@gmail.com>
2019-06-14 14:34:31 -0400

    Sfg

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -136,7 +136,12 @@ L.TrigUtil = {
 
   calcAngleDegrees: function(x, y) {
     return Math.atan2(y, x) * 180 / Math.PI;
-  }
+  },
+
+  // Converts from degrees to radians.
+ radians: function(degrees) {
+  return degrees * Math.PI / 180;
+}
 
 };
 L.DistortableImageOverlay = L.ImageOverlay.extend({
