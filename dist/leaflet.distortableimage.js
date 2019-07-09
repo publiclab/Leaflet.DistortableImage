402ae5440dc8958fef91fd51367175c5133694a0
sashadev-sky <sboginsky17@gmail.com>
2019-07-09 06:55:02 -0400

    standardize quotations

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -2400,7 +2400,7 @@ L.DistortableImage.Keymapper = L.Control.extend({
 L.Map.mergeOptions({ boxSelector: true, boxZoom: false });
 
 /** 
- * pretty much all Leaflet 1.5.1 source code. Overriden so that its a selection box with our `L.DistortableCollection` class 
+ * primarily Leaflet 1.5.1 source code. Overriden so that its a selection box with our `L.DistortableCollection` class 
  * instead of a zoom box. 
  * */
 
@@ -2410,15 +2410,15 @@ L.Map.BoxSelector = L.Map.BoxZoom.extend({
     this._container = map._container;
     this._pane = map._panes.overlayPane;
     this._resetStateTimeout = 0;
-    map.on("unload", this._destroy, this);
+    map.on('unload', this._destroy, this);
   },
 
   addHooks: function() {
-    L.DomEvent.on(this._container, "mousedown", this._onMouseDown, this);
+    L.DomEvent.on(this._container, 'mousedown', this._onMouseDown, this);
   },
 
   removeHooks: function() {
-    L.DomEvent.off(this._container, "mousedown", this._onMouseDown, this);
+    L.DomEvent.off(this._container, 'mousedown', this._onMouseDown, this);
   },
 
   moved: function() {
@@ -2467,10 +2467,10 @@ L.Map.BoxSelector = L.Map.BoxZoom.extend({
     if (!this._moved) {
       this._moved = true;
 
-      this._box = L.DomUtil.create("div", "leaflet-zoom-box", this._container);
-      L.DomUtil.addClass(this._container, "leaflet-crosshair");
+      this._box = L.DomUtil.create('div', 'leaflet-zoom-box', this._container);
+      L.DomUtil.addClass(this._container, 'leaflet-crosshair');
 
-      this._map.fire("boxzoomstart");
+      this._map.fire('boxzoomstart');
     }
 
     this._point = this._map.mouseEventToContainerPoint(e);
@@ -2480,14 +2480,14 @@ L.Map.BoxSelector = L.Map.BoxZoom.extend({
 
     L.DomUtil.setPosition(this._box, this._bounds.min);
 
-    this._box.style.width = size.x + "px";
-    this._box.style.height = size.y + "px";
+    this._box.style.width = size.x + 'px';
+    this._box.style.height = size.y + 'px';
   },
 
   _finish: function() {
     if (this._moved) {
       L.DomUtil.remove(this._box);
-      L.DomUtil.removeClass(this._container, "leaflet-crosshair");
+      L.DomUtil.removeClass(this._container, 'leaflet-crosshair');
     }
 
     L.DomUtil.enableTextSelection();
@@ -2522,7 +2522,7 @@ L.Map.BoxSelector = L.Map.BoxZoom.extend({
       this._map.layerPointToLatLng(this._bounds.getTopRight())
     );
 
-    this._map.fire("boxzoomend", { boxZoomBounds: bounds });
+    this._map.fire('boxzoomend', { boxZoomBounds: bounds });
   }
 });
 
