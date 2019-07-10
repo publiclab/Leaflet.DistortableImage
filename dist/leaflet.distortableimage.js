472302d566bbe3732540991ab179a80c9959e3b6
sashadev-sky <sboginsky17@gmail.com>
2019-07-10 13:24:56 -0400

    undo autoformatting

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -8,23 +8,11 @@ L.DomUtil = L.extend(L.DomUtil, {
        *     https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function#M.C3.B6bius'_homogeneous_coordinates_in_projective_geometry
        */
       matrix = [
-        m[0],
-        m[3],
-        0,
-        m[6],
-        m[1],
-        m[4],
-        0,
-        m[7],
-        0,
-        0,
-        1,
-        0,
-        m[2],
-        m[5],
-        0,
-        m[8]
-      ],
+        m[0], m[3], 0, m[6],
+        m[1], m[4], 0, m[7],
+        0, 0, 1, 0,
+        m[2], m[5], 0, m[8]
+	  ],
       str = is3d ? "matrix3d(" + matrix.join(",") + ")" : "";
 
     if (!is3d) {
