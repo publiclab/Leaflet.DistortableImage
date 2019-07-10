f950f0a05e51af6eb493f0bc5d8009b5b7f6054a
sashadev-sky <sboginsky17@gmail.com>
2019-07-10 07:55:55 -0400

    Revert "attempt"
    This reverts commit bdd94eedae1bde2cf8a69cd3f4dbc43a6882ceb3.

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -1199,9 +1199,8 @@ L.RotateScaleHandle = L.EditHandle.extend({
 		})
 	},
 
-	_onHandleDrag: function(e) {
+	_onHandleDrag: function() {
 		var overlay = this._handled,
-			map = this._map,
 			edit = overlay.editing,
 			formerLatLng = overlay.getCorner(this._corner),
 			newLatLng = this.getLatLng(),
@@ -1215,69 +1214,17 @@ L.RotateScaleHandle = L.EditHandle.extend({
 		  checks whether the "edgeMinWidth" property is set and tracks the minimum edge length;
 		  this enables preventing scaling to zero, but we might also add an overall scale limit
 		*/		
-		// if (overlay.hasOwnProperty('edgeMinWidth')){
-		// 	var edgeMinWidth = overlay.edgeMinWidth,
-		// 	    w = L.latLng(overlay.getCorner(0)).distanceTo(overlay.getCorner(1)),
-		// 			h = L.latLng(overlay.getCorner(1)).distanceTo(overlay.getCorner(2));
-		// 	if ((w > edgeMinWidth && h > edgeMinWidth) || scale > 1) {
-		// 		edit._scaleBy(scale);
-		// 	}
-		// } 
-
-		// overlay.fire('update');
-		// edit._updateToolbarPos();
-
 		if (overlay.hasOwnProperty('edgeMinWidth')){
 			var edgeMinWidth = overlay.edgeMinWidth,
-				// sw = L.CRS.latLngToPoint(overlay.getCorner(2), map.getZoom()),
-				// ne = L.CRS.latLngToPoint(overlay.getCorner(1), map.getZoom()),
-      			ne = map.latLngToContainerPoint(overlay.getCorner(1)),
-      			sw = map.latLngToContainerPoint(overlay.getCorner(2)),
-				// corner1 = map.project(overlay.getCorner(0), map.getZoom()),
-				// corner2 = map.project(overlay.getCorner(1), map.getZoom()),
-				  // corner2 = map.latLngToContainerPoint(overlay.getCorner(1)),
-				  bounds = L.bounds(sw, ne);
-     			// w;
-			// w = corner2.x - corner1.x;
-			// h = Math.abs(corner1.y - corner2.y);
-			// if (scale > 0) {
-			// 	overlay.scale = scale;
-			// 	edit._scaleBy2(scale);
-				// overlay.fire("update");
-            	// edit._updateToolbarPos();
-			// }
-			window.boundsa = bounds;
-			// var intersect = corner1.intersect
-			if ((bounds.getSize().x > 16 && bounds.getSize().y > 16) || scale > 1 ) {
-				overlay.scale = scale;
+			    w = L.latLng(overlay.getCorner(0)).distanceTo(overlay.getCorner(1)),
+					h = L.latLng(overlay.getCorner(1)).distanceTo(overlay.getCorner(2));
+			if ((w > edgeMinWidth && h > edgeMinWidth) || scale > 1) {
 				edit._scaleBy(scale);
-				// this.updateHandle();
-          		// overlay.fire("update");
-          		// edit._updateToolbarPos();
-			} else {
-				// L.DomUtil.debounce(function() {
-					// edit._disable();
-					// this.dragging.disable();
-					// this._unbindListeners();
-					// var newZoom = map.getBoundsZoom(new L.latLngBounds(overlay.getCorner(2), overlay.getCorner(1)));
-					// map.zoomIn(0.5);
-					// map.panInsideBounds(new L.latLngBounds(overlay.getCorner(2), overlay.getCorner(1)));
-					// map.setZoomAround(overlay.getCenter(), newZoom);
-					// map.flyToBounds();
-					// return false;
-					L.DomEvent.stop(e);
-					return L.Util.falseFn();
-				// }, 350, true);
-				// overlay._animateZoom();
-				// map.zoomIn();
-				// map.fitBounds()
 			}
-		}
+		} 
 
-		this.updateHandle();
 		overlay.fire('update');
 		edit._updateToolbarPos();
-		// overlay.updateHandle();
 	},
 
 	updateHandle: function() {
