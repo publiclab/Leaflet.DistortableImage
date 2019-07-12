9f1da96d15dab37805ceea68d1b118d677fe3989
sashadev-sky <sboginsky17@gmail.com>
2019-07-12 01:38:58 -0400

    put back map mixin

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -64,6 +64,15 @@ L.ImageUtil = {
   }
 };
 
+L.Map.include({
+  _newLayerPointToLatLng: function(point, newZoom, newCenter) {
+    var topLeft = L.Map.prototype._getTopLeftPoint
+      .call(this, newCenter, newZoom)
+      .add(L.Map.prototype._getMapPanePos.call(this));
+    return this.unproject(point.add(topLeft), newZoom);
+  }
+});
+
 L.MatrixUtil = {
 
 	// Compute the adjugate of m
