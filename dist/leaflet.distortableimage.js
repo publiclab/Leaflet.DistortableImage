f704bacb2be03cd08ab42409ac28610830ed52ad
rexagod <rexagod@gmail.com>
2019-07-12 07:11:38 +0530

    improve on @sashadev-sky's review
    refer: https://github.com/publiclab/Leaflet.DistortableImage/pull/169#pullrequestreview-236401037

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -142,99 +142,64 @@ L.TrigUtil = {
 L.tools = L.tools || {};
 
 L.tools.EXIF = function getEXIFdata(ref, overlay) {
-    var GPS = ref.exifdata,
-        altitude, lat, lng;
+  var GPS = ref.exifdata,
+    lat,
+    lng;
 
-    /* If the lat/lng is available. */
-    if (
-        typeof GPS.GPSLatitude !== "undefined" &&
-        typeof GPS.GPSLongitude !== "undefined"
-    ) {
-        // sadly, encoded in [degrees,minutes,seconds]
-        // primitive value = GPS.GPSLatitude[x].numerator
-        lat =
-            GPS.GPSLatitude[0] +
-            GPS.GPSLatitude[1] / 60 +
-            GPS.GPSLatitude[2] / 3600;
-        lng =
-            GPS.GPSLongitude[0] +
-            GPS.GPSLongitude[1] / 60 +
-            GPS.GPSLongitude[2] / 3600;
+  if (
+    typeof GPS.GPSLatitude !== "undefined" &&
+    typeof GPS.GPSLongitude !== "undefined"
+  ) {
+    lat =
+      GPS.GPSLatitude[0] + GPS.GPSLatitude[1] / 60 + GPS.GPSLatitude[2] / 3600;
+    lng =
+      GPS.GPSLongitude[0] +
+      GPS.GPSLongitude[1] / 60 +
+      GPS.GPSLongitude[2] / 3600;
 
-        if (GPS.GPSLatitudeRef !== "N") {
-            lat = lat * -1;
-        }
-        if (GPS.GPSLongitudeRef === "W") {
-            lng = lng * -1;
-        }
+    if (GPS.GPSLatitudeRef !== "N") {
+      lat = lat * -1;
     }
-
-    // Attempt to use GPS compass heading; will require
-    // some trig to calculate corner points, which you can find below:
-
-    var angle = 0;
-    // "T" refers to "True north", so -90.
-    if (GPS.GPSImgDirectionRef === "T") {
-        angle =
-            (Math.PI / 180) *
-            (GPS.GPSImgDirection.numerator / GPS.GPSImgDirection.denominator - 90);
+    if (GPS.GPSLongitudeRef === "W") {
+      lng = lng * -1;
     }
-    // "M" refers to "Magnetic north"
-    else if (GPS.GPSImgDirectionRef === "M") {
-        angle =
-            (Math.PI / 180) *
-            (GPS.GPSImgDirection.numerator / GPS.GPSImgDirection.denominator - 90);
-    } else {
-        console.log("No compass data found");
-    }
+  }
 
-    console.log("Orientation:", GPS.Orientation);
+  var angle = 0;
 
-    /* If there is orientation data -- i.e. landscape/portrait etc */
-    if (GPS.Orientation === 6) {
-        //CCW
-        angle += (Math.PI / 180) * -90;
-    } else if (GPS.Orientation === 8) {
-        //CW
-        angle += (Math.PI / 180) * 90;
-    } else if (GPS.Orientation === 3) {
-        //180
-        angle += (Math.PI / 180) * 180;
-    }
+  if (GPS.GPSImgDirectionRef === "T" || GPS.GPSImgDirectionRef === "M") {
+    angle =
+      (Math.PI / 180) *
+      (GPS.GPSImgDirection.numerator / GPS.GPSImgDirection.denominator - 90);
+  }
 
-    /* If there is altitude data */
-    if (
-        typeof GPS.GPSAltitude !== "undefined" &&
-        typeof GPS.GPSAltitudeRef !== "undefined"
-    ) {
-        // Attempt to use GPS altitude:
-        // (may eventually need to find EXIF field of view for correction)
-        if (
-            typeof GPS.GPSAltitude !== "undefined" &&
-            typeof GPS.GPSAltitudeRef !== "undefined"
-        ) {
-            altitude =
-                GPS.GPSAltitude.numerator / GPS.GPSAltitude.denominator +
-                GPS.GPSAltitudeRef;
-        } else {
-            altitude = 0; // none
-        }
-    }
+  if (GPS.Orientation === 6) {
+    angle += (Math.PI / 180) * -90;
+  } else if (GPS.Orientation === 8) {
+    angle += (Math.PI / 180) * 90;
+  } else if (GPS.Orientation === 3) {
+    angle += (Math.PI / 180) * 180;
+  }
 
-    var panTo = new L.latLng(lat, lng);
+  var panTo = L.latLng(lat, lng);
 
-    var x_diff = overlay._corners[0].lng - overlay._corners[1].lng; // width -- a
-    var y_diff = overlay._corners[0].lat - overlay._corners[2].lat; // height -- b
+  var x_diff = overlay.getCorner(0).lng - overlay.getCorner(1).lng; // width
+  var y_diff = overlay.getCorner(0).lat - overlay.getCorner(2).lat; // height
 
-    overlay._corners[0] = new L.latLng(lat + y_diff / 2, lng + x_diff / 2);
-    overlay._corners[1] = new L.latLng(lat + y_diff / 2, lng - x_diff / 2);
-    overlay._corners[2] = new L.latLng(lat - y_diff / 2, lng + x_diff / 2);
-    overlay._corners[3] = new L.latLng(lat - y_diff / 2, lng - x_diff / 2);
+    //  _corners: array element holding a <LatLng> object ([])
+    //  changes made to _corners, unlike getCorners (value return only)
+    // will actually mutate corners, which is what we need to do
 
-    overlay.editing._rotateBy(angle);
+  overlay._corners[0] = L.latLng(lat + y_diff / 2, lng + x_diff / 2);
+  overlay._corners[1] = L.latLng(lat + y_diff / 2, lng - x_diff / 2);
+  overlay._corners[2] = L.latLng(lat - y_diff / 2, lng + x_diff / 2);
+  overlay._corners[3] = L.latLng(lat - y_diff / 2, lng - x_diff / 2);
 
-    overlay._map.setView(panTo, 14);
+  overlay.editing._rotateBy(angle);
+
+  overlay._map.setView(panTo, 13);
 };
+
 L.EditHandle = L.Marker.extend({
   initialize: function(overlay, corner, options) {
     var markerOptions,
