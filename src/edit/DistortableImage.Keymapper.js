403e4d95668e014b70a7043db47a55a8843ae1ce
sashadev-sky <sboginsky17@gmail.com>
2019-07-26 23:47:21 -0400

    fix so that enable works

--- src/edit/DistortableImage.Keymapper.js
+++ src/edit/DistortableImage.Keymapper.js
@@ -9,6 +9,7 @@ L.DistortableImage.Keymapper = L.Handler.extend({
     this._position = this._params.position || 'topright';
     window.keymapper_instances = window.keymapper_instances ? window.keymapper_instances + 1 : 1;
     this._instancesCheck(window.keymapper_instances);
+    this._keymapper = L.control({ position: this._position });
   },
 
   _instancesCheck: function (len) { 
@@ -20,16 +21,21 @@ L.DistortableImage.Keymapper = L.Handler.extend({
   },
 
   addHooks: function () {
-    this._setMapper();
+    this.enable();
    },
 
   removeHooks: function () { 
     this.disable();
   },
 
-  disable: function () {    
-    var el = document.getElementById('l-container');
-    el.parentNode.removeChild(el);
+  enable: function () {
+    this._setMapper();
+  },
+
+  disable: function () {  
+    if (this._keymapper) {
+     L.DomUtil.remove(this._keymapper._container);
+    } 
   },
 
   _disableAllButOne: function () { 
@@ -38,20 +44,27 @@ L.DistortableImage.Keymapper = L.Handler.extend({
       generated_instances_array[i].parentNode.removeChild(generated_instances_array[i]);
     }
   },
+
+  _createToggler: function () {
+    this._toggler = L.DomUtil.create('button');
+    this._toggler.setAttribute('id', 'toggle-keymapper');
+    this._toggler.innerHTML = 
+      '<svg>' +
+      '<use xlink:href="#keyboard-open"></use>' +
+      '<symbol viewBox="0 0 25 25" id="keyboard-open" xmlns="http://www.w3.org/2000/svg"><path d="M12 23l4-4H8l4 4zm7-15h-2V6h2v2zm0 3h-2V9h2v2zm-3-3h-2V6h2v2zm0 3h-2V9h2v2zm0 4H8v-2h8v2zM7 8H5V6h2v2zm0 3H5V9h2v2zm1-2h2v2H8V9zm0-3h2v2H8V6zm3 3h2v2h-2V9zm0-3h2v2h-2V6zm9-3H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></symbol>' +
+      '<symbol viewBox="0 0 20 20" id="arrow-collapse" xmlns="http://www.w3.org/2000/svg"><path d="M2.6 19l4.5-4.5v3.6h2v-7h-7v2h3.6l-4.5 4.5L2.6 19zm15.5-9.9v-2h-3.6L19 2.6l-1.4-1.4-4.5 4.5V2.1h-2v7h7z"/></symbol>' +
+      '</svg>';
+    L.DomEvent.on(this._toggler, 'click', this._toggleKeymapper, this);
+    return this._toggler;
+  },
   
   _setMapper: function () {
-    this._keymapper = L.control({ position: this._position });
-    this._keymapper.onAdd = function () {
+    var button = this._createToggler();
+    this._container = this._keymapper.onAdd = function () {
       var el_wrapper = L.DomUtil.create("div", "l-container-hide");
       el_wrapper.setAttribute('id', 'l-container');
-      el_wrapper.innerHTML =
-        "<button id='toggle-keymapper'>" +
-        '<svg>' +
-        '<use xlink:href="#keyboard-open"></use>' +
-        '<symbol viewBox="0 0 25 25" id="keyboard-open" xmlns="http://www.w3.org/2000/svg"><path d="M12 23l4-4H8l4 4zm7-15h-2V6h2v2zm0 3h-2V9h2v2zm-3-3h-2V6h2v2zm0 3h-2V9h2v2zm0 4H8v-2h8v2zM7 8H5V6h2v2zm0 3H5V9h2v2zm1-2h2v2H8V9zm0-3h2v2H8V6zm3 3h2v2h-2V9zm0-3h2v2h-2V6zm9-3H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></symbol>' +
-        '<symbol viewBox="0 0 20 20" id="arrow-collapse" xmlns="http://www.w3.org/2000/svg"><path d="M2.6 19l4.5-4.5v3.6h2v-7h-7v2h3.6l-4.5 4.5L2.6 19zm15.5-9.9v-2h-3.6L19 2.6l-1.4-1.4-4.5 4.5V2.1h-2v7h7z"/></symbol>' +
-        '</svg>' +
-        '</button>' +
+      el_wrapper.appendChild(button);
+      el_wrapper.insertAdjacentHTML('beforeend', 
         '<div id="keymapper-wrapper" style="display:none">' +
         '<table><tbody>' +
         "<tr><td><center><span id='keymapper-heading'>Keymappings</span></center></td></tr>" +
@@ -66,17 +79,14 @@ L.DistortableImage.Keymapper = L.Handler.extend({
         '<tr><td><kbd>delete</kbd> , <kbd>backspace</kbd>: <span>Delete</span></td></tr>' +
         '<tr><td><kbd>caps</kbd>: <span>Rotate</span></td></tr>' +
         '</div>' +
-        '</tbody></table>';
+        '</tbody></table>');
       return el_wrapper;
     };
     this._keymapper.addTo(this._map);
-  }
-});
+  },
 
-window.onload = function () {
-  if (document.getElementById('l-container')) {
-    document.getElementById('toggle-keymapper').addEventListener('click', function (e) {
-      L.DomEvent.stop(e);
+  _toggleKeymapper: function (e) {
+    L.DomEvent.stop(e);
       var container = document.getElementById('l-container');
       var xlink = document.querySelector("#toggle-keymapper > svg > use:nth-child(1)");
       var keymapWrap = document.getElementById('keymapper-wrapper');
@@ -88,8 +98,8 @@ window.onload = function () {
       container.className = newClass;
       xlink.setAttribute('xlink:href', newXlink);
       keymapWrap.style.display = newStyle;
-    });
-  }};
+  },
+});
 
 L.distortableImage.keymapper = function (options) {
   return new L.DistortableImage.Keymapper(options);
