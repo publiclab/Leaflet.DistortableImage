185b50d6b9cab735512b96298afea6eb9bcf4c43
sashadev-sky <sboginsky17@gmail.com>
2019-08-20 22:44:24 -0400

    add new line to file

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -1400,6 +1400,7 @@ L.EditAction = L.Toolbar2.Action.extend({
 L.editAction = function(map, overlay, options) {
   return new L.EditAction(map, overlay, options);
 };
+
 L.DeleteAction = L.EditAction.extend({
   initialize: function(map, overlay, options) {
     var use = 'delete_forever';
