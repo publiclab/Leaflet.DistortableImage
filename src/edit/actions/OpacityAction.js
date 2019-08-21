d98e41d39c75276ea7b39fe0f05040e3223b5447
sashadev-sky <sboginsky17@gmail.com>
2019-08-20 21:34:52 -0400

    fix opacity tool

--- src/edit/actions/OpacityAction.js
+++ src/edit/actions/OpacityAction.js
@@ -26,8 +26,8 @@ L.OpacityAction = L.EditAction.extend({
   addHooks: function() {
     var edit = this._overlay.editing;
 
-    L.IconUtil.toggleXlink(this._link, 'opacity_empty', 'opacity');
-    L.IconUtil.toggleTooltip(this._link, 'Make Image Opaque', 'Make Image Transparent');
+    L.IconUtil.toggleXlink(this._link, 'opacity', 'opacity_empty');
+    L.IconUtil.toggleTooltip(this._link, 'Make Image Transparent', 'Make Image Opaque');
     edit._toggleOpacity();
   }
 });
