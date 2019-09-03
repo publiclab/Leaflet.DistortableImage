9175f6e14b0977884b025c333c60127873552035
sashadev-sky <sboginsky17@gmail.com>
2019-09-03 19:13:20 -0400

    updates

--- src/edit/RotateScaleHandle.js
+++ src/edit/RotateScaleHandle.js
@@ -3,6 +3,7 @@ L.RotateScaleHandle = L.EditHandle.extend({
     TYPE: 'rotateScale',
     icon: L.icon({
       iconUrl:
+        // eslint-disable-next-line max-len
         'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAklEQVR4AewaftIAAAHiSURBVMXBa3HbShgA0PMp/1sCCo8oCEpgTaCXgIXAJiDzyCJoAUTm4UVQAns1Y8+snWnTvJyeE16hkjDgDrfoNTMKcpC9UPiLSo8JyetkjEHxjPCMyoS199kFoz8Iv1HpMaN3qWDCHoegOKkkRwnJpRmroHgiPFEZ8IBekzEGxQtUEhKSS/fB7Ew4U+lxcGkVZG9QWWPSFAxBcdK59KApuA+yNwp2uEdx1GN25sZJZULSfAtm77SlbNjju6MvG75u+WHRWVR6rDVjMPsgwYyVZl3pLTpHkyYHOx8syMiayaJzlDTZ9YyaZNFVkiYH2ZUEBcVJJXVImuz6Js3Qofe59pq7DoOTILu+g+a288mCouk7/1iH4qTS+2QdDppbV1ZJmrnDXnPnc5UOs2Z0fUmTuyBr+krvSioJyUmQO0dZM7mepMkWnaNRkyrJB6uskTSjxY3Fll8bvmJwlDb83FJ8gMqAB80uyBY3Trb82PAfvjj6vuHnluIdKgMeNXOwctK5NKBoHitrb1RJeHRp5Ux4ojLg0aWMHGQvUOkxIWkKVsHsTPiNSo8HDC5lZIsgO6n0uMUdRvQuFQxB8UR4RmXC2vvsgtEfhL+o9JiQvE7GGBTPCK9QSUjoMWgKDthjDrIX+h/k0I7gth6N5gAAAABJRU5ErkJggg==',
       iconSize: [32, 32],
       iconAnchor: [16, 16],
@@ -18,9 +19,7 @@ L.RotateScaleHandle = L.EditHandle.extend({
     var angle = this.calculateAngleDelta(formerLatLng, newLatLng);
     var scale = this._calculateScalingFactor(formerLatLng, newLatLng);
 
-    if (angle !== 0) { 
-      overlay.rotateBy(angle); 
-    }
+    if (angle !== 0) { overlay.rotateBy(angle); }
 
     if (!edgeMinWidth) { edgeMinWidth = 50; } /* just in case */
     var corner1 = map.latLngToContainerPoint(overlay.getCorner(0));
@@ -36,7 +35,6 @@ L.RotateScaleHandle = L.EditHandle.extend({
   },
 
   updateHandle: function() {
-    // this._handled.setCorner(this._corner, )
     this.setLatLng(this._handled.getCorner(this._corner));
   },
 });
