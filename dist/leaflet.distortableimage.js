737772f906fe39ab3e2a9d3a4deff5277632f8d0
sashadev-sky <sboginsky17@gmail.com>
2019-09-15 02:07:33 -0400

    update

--- dist/leaflet.distortableimage.js
+++ dist/leaflet.distortableimage.js
@@ -1748,6 +1748,89 @@ L.StackAction = L.EditAction.extend({
   },
 });
 
+L.DistortableImage = L.DistortableImage || {};
+L.distortableImage = L.DistortableImage;
+
+L.DistortableImage.action_map = {};
+
+L.DistortableImage.PopupBar = L.Toolbar2.Popup.extend({
+  options: {
+    anchor: [0, -10],
+  },
+});
+
+L.distortableImage.popupBar = function(latlng, options) {
+  return new L.DistortableImage.PopupBar(latlng, options);
+};
+
+L.DistortableImageOverlay.addInitHook(function() {
+  /** Default actions */
+  this.ACTIONS = [
+    L.ScaleAction,
+    L.DistortAction,
+    L.RotateAction,
+    L.FreeRotateAction,
+    L.LockAction,
+    L.OpacityAction,
+    L.BorderAction,
+    L.StackAction,
+    L.ExportAction,
+    L.DeleteAction,
+  ];
+
+  var a = this.options.actions ? this.options.actions : this.ACTIONS;
+
+  this.editing = L.distortableImage.edit(this, {actions: a});
+});
+
+
+L.distortableImage = L.DistortableImage || {};
+L.distortableImage = L.DistortableImage;
+
+L.DistortableImage.group_action_map = {};
+
+L.UnlocksAction = L.EditAction.extend({
+  initialize: function(map, overlay, options) {
+    options = options || {};
+    options.toolbarIcon = {
+      svg: true,
+      html: 'unlock',
+      tooltip: 'Unlock Images',
+    };
+
+    L.DistortableImage.group_action_map.u = '_unlockGroup';
+    L.EditAction.prototype.initialize.call(this, map, overlay, options);
+  },
+
+  addHooks: function() {
+    var edit = this._overlay.editing;
+    edit._unlockGroup();
+  },
+});
+
+L.DistortableImage.ControlBar = L.Toolbar2.Control.extend({
+  options: {
+  },
+});
+
+L.distortableImage.controlBar = function(options) {
+  return new L.DistortableImage.ControlBar(options);
+};
+
+/** addInitHooks run before onAdd */
+L.DistortableCollection.addInitHook(function() {
+  this.ACTIONS = [
+    L.ExportAction,
+    L.DeleteAction,
+    L.LockAction,
+    L.UnlocksAction,
+  ];
+
+  var a = this.options.actions ? this.options.actions : this.ACTIONS;
+
+  this.editing = L.distortableCollection.edit(this, {actions: a});
+});
+
 /* eslint-disable valid-jsdoc */
 L.DistortableImage = L.DistortableImage || {};
 
