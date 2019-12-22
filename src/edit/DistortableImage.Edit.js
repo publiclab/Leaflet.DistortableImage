5c1a2469153db5eee5bae03b2eb00f4bcf7962d8
Sparks <zaharus000@mail.ru>
2019-12-22 15:20:21 +0700

    Update DistortableImage.Edit.js

--- src/edit/DistortableImage.Edit.js
+++ src/edit/DistortableImage.Edit.js
@@ -195,10 +195,8 @@ L.DistortableImage.Edit = L.Handler.extend({
       this._removeToolbar();
       this.editActions.push(value);
       this._addToolbar();
-      return this;
-    } else {
-      return false;
     }
+    return this;
   },
 
   hasTool: function(value) {
