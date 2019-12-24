86bd88d9c8060573060da5a49b7c1a10a41fb6c3
jywarren <jeff@unterbahn.com>
2019-12-24 13:24:13 -0500

    reverting unintended boxcollect changes

--- src/edit/DistortableCollection.Edit.js
+++ src/edit/DistortableCollection.Edit.js
@@ -104,7 +104,7 @@ L.DistortableCollection.Edit = L.Handler.extend({
       singleclickon: this._singleClickListeners,
       singleclickoff: this._resetClickListeners,
       singleclick: this._singleClick,
-      boxzoomend: this._addCollections,
+      boxcollectend: this._addCollections,
     }, this);
 
     this._group.editable = true;
@@ -127,7 +127,7 @@ L.DistortableCollection.Edit = L.Handler.extend({
       singleclickon: this._singleClickListeners,
       singleclickoff: this._resetClickListeners,
       singleclick: this._singleClick,
-      boxzoomend: this._addCollections,
+      boxcollectend: this._addCollections,
     }, this);
 
     this._decollectAll();
@@ -183,7 +183,7 @@ L.DistortableCollection.Edit = L.Handler.extend({
 
     if (e) { oe = e.originalEvent; }
     /**
-     * prevents image deselection following the 'boxzoomend' event - note 'shift' must not be released until dragging is complete
+     * prevents image deselection following the 'boxcollectend' event - note 'shift' must not be released until dragging is complete
      * also prevents deselection following a click on a disabled img by differentiating it from the map
      */
     if (oe && (oe.shiftKey || oe.target instanceof HTMLImageElement)) {
@@ -227,7 +227,7 @@ L.DistortableCollection.Edit = L.Handler.extend({
   },
 
   _addCollections: function(e) {
-    var box = e.boxZoomBounds;
+    var box = e.boxCollectBounds;
     var map = this._group._map;
 
     this._group.eachLayer(function(layer) {
