53cb4d7d99b6f700e02568b175201f5030ff35b5
sashadev-sky <sboginsky17@gmail.com>
2020-01-08 04:36:34 -0500

    fix linting error

--- src/edit/handles/LockHandle.js
+++ src/edit/handles/LockHandle.js
@@ -4,7 +4,6 @@ L.LockHandle = L.EditHandle.extend({
     icon: L.icon({
       // eslint-disable-next-line max-len
       iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAklEQVR4AewaftIAAAD8SURBVO3BPU7CYAAA0AdfjIcQlRCQBG7C3gk2uIPG2RC3Dk16Gz0FTO1WZs/gwGCMP/2+xsSl7+n1er1Iz9LtRQjaPeMeO+TinLDCJV78YqjdA04YodKuxhUaPGoRxMmxwRQZSt87Yo4KExGCeAUyLLFB4bMacxywEClIU2KDKXbInTUYo8JCgoFuGoxQO5uiwY1EA91VmDqrcKeDoX8WdNNgjApvmGGLXKIgXY0xGkxQYItrrFFIEKQ5Yo4KEx9yrDFDhlKkIF6NOQ5Y+KpAhiXWKEQI4pxwiwoLPyuxwQw75FoE7fZYocFEuwI7jHCBV39gL92TXq/Xi/AOcmczZmaIMScAAAAASUVORK5CYII=',
-      interactive: false,
       iconSize: [32, 32],
       iconAnchor: [16, 16],
     }),
@@ -18,24 +17,36 @@ L.LockHandle = L.EditHandle.extend({
 
   _bindListeners: function() {
     var icon = this.getElement();
-    var img = this._handled.getElement();
+    var ov = this._handled;
+    var img = ov.getElement();
 
-    this.on('contextmenu', L.DomEvent.stop, this);
-    L.DomEvent.on(icon, 'mousedown', this._tooltipOn, this);
-    L.DomEvent.on(icon, 'mouseout mouseup', this._tooltipOff, this);
-    L.DomEvent.on(img, 'mousedown', this._tooltipOn, this);
-    L.DomEvent.on(img, 'mouseout mouseup', this._tooltipOff, this);
+    L.EditHandle.prototype._bindListeners.call(this);
+
+    L.DomEvent.on(icon, {
+      mousedown: this._tooltipOn,
+      mouseup: this._tooltipOff,
+    }, this);
+
+    L.DomEvent.on(img, 'mousedown', this._tooltipOn, ov);
+    L.DomEvent.on(img, 'mouseout mouseup dragstart', this._tooltipOff, ov);
+    L.DomEvent.on(document, 'mouseleave', this._tooltipOff, ov);
   },
 
   _unbindListeners: function() {
     var icon = this.getElement();
-    var img = this._handled.getElement();
+    var ov = this._handled;
+    var img = ov.getElement();
 
-    this.off('contextmenu', L.DomEvent.stop, this);
-    L.DomEvent.off(icon, 'mousedown', this._tooltipOn, this);
-    L.DomEvent.off(icon, 'mouseout mouseup', this._tooltipOff, this);
-    L.DomEvent.off(img, 'mousedown', this._tooltipOn, this);
-    L.DomEvent.off(img, 'mouseout mouseup', this._tooltipOff, this);
+    L.EditHandle.prototype._bindListeners.call(this);
+
+    L.DomEvent.off(icon, {
+      mousedown: this._tooltipOn,
+      mouseup: this._tooltipOff,
+    }, this);
+
+    L.DomEvent.off(img, 'mousedown', this._tooltipOn, ov);
+    L.DomEvent.off(img, 'mouseout mouseup dragstart', this._tooltipOff, ov);
+    L.DomEvent.off(document, 'mouseleave', this._tooltipOff, ov);
   },
 
   /* cannot be dragged */
@@ -47,35 +58,37 @@ L.LockHandle = L.EditHandle.extend({
   },
 
   _tooltipOn: function(e) {
-    if (e && e.type === 'mousedown' && !e.shiftKey) {
-      var scope = L.DomUtil.hasClass(e.target, 'leaflet-image-layer') ?
-          this._handled :
-          this;
+    if (e.shiftKey) { return; }
 
+    this._timer = setTimeout(L.bind(function() {
       if (this._timeout) { clearTimeout(this._timeout); }
-      this._timer = setTimeout(L.bind(function() {
-        if (!this.getTooltip()) {
-          this.bindTooltip('Locked!', {permanent: true});
-        }
-        this.openTooltip();
-      }, scope), 500);
-    }
+
+      if (!this.getTooltip()) {
+        this.bindTooltip('Locked!', {permanent: true});
+      }
+
+      this.openTooltip();
+    }, this), 500);
   },
 
   _tooltipOff: function(e) {
-    var scope = this;
+    if (e.shiftKey) { return; }
 
-    if (e) {
-      scope = L.DomUtil.hasClass(e.target, 'leaflet-image-layer') ?
-        this._handled :
-        this;
+    var scope = this._handled ?
+      this._handled.editing._lockHandles :
+      this.editing._lockHandles;
+
+    if (e.currentTarget === document) {
+      scope.eachLayer(function(handle) {
+        handle.closeTooltip();
+      });
     }
 
     if (this._timer) { clearTimeout(this._timer); }
 
     this._timeout = setTimeout(L.bind(function() {
-      this.closeTooltip();
-    }, scope), 400);
+      if (e.type !== 'mouseout') { this.closeTooltip(); }
+    }, this), 400);
   },
 });
 
