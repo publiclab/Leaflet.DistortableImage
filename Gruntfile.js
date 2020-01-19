ab40aab1810cd99596b4f89bdb15818c351ff4b0
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-19 19:50:01 +0100

    ADD: "watch" Grunt task to run tests on change

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,6 +111,16 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      source: {
+        files: ['src/**/*.js', 'test/**/*.js', 'Gruntfile.js'],
+        tasks: ['jshint', 'karma:development:start']
+      }
     }
   });
 
