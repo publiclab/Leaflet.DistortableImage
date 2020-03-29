3b80a0b8095ff97c0d1eeabe491e06b45b3cb14f
Sasha Boginsky <41092741+sashadev-sky@users.noreply.github.com>
2020-03-29 07:30:39 -0400

    Rotation API (#579)
    * Audit package-lock
    
    * Update rotation angle setters and getters

--- test/src/DistortableImageOverlaySpec.js
+++ test/src/DistortableImageOverlaySpec.js
@@ -22,8 +22,17 @@ describe('L.DistortableImageOverlay', function() {
       ],
     }).addTo(map);
 
+    overlay2 = L.distortableImageOverlay('/examples/example.png', {
+      corners: [
+        L.latLng(41.7934, -87.6052),
+        L.latLng(41.7934, -87.5852),
+        L.latLng(41.7834, -87.6052),
+        L.latLng(41.7834, -87.5852),
+      ],
+    }).addTo(map);
+
     /* Forces the image to load before any tests are run. */
-    L.DomEvent.on(overlay._image, 'load', function() { 
+    L.DomEvent.on(overlay.getElement(), 'load', function() {
       expect(map.getMaxZoom()).to.equal(Infinity); // before adding any background layers
       done();
     });
@@ -34,7 +43,7 @@ describe('L.DistortableImageOverlay', function() {
   });
 
   describe('#basic initialization', function() {
-    // we need a maxZoom of 24 so that images of very small areas may be distorted against 
+    // we need a maxZoom of 24 so that images of very small areas may be distorted against
     // one another even if the background imagery is not good enough to be useful.
     it('should add Google tile base layer via Google Mutant library, with maxZoom of 24', function(done) {
       map.addGoogleMutant();
@@ -182,6 +191,114 @@ describe('L.DistortableImageOverlay', function() {
       var center = overlay.getCenter();
       expect(Math.round(overlay.getCenter().lat)).to.equal(Math.round(center.lat));
       expect(Math.round(overlay.getCenter().lng)).to.equal(Math.round(center.lng));
+    });
+  });
+
+  describe('#rotateBy', function() {
+    it('Should not rotate the image when passed a value of 0 or 360', function() {
+      var corners = overlay.getCorners();
+
+      overlay.rotateBy(0);
+      expect(corners).to.eql(overlay.getCorners());
+
+      overlay.rotateBy(360);
+      expect(corners).to.eql(overlay.getCorners());
+    });
+
+    it('Should rotate the image by the specified units, otherwise by degrees', function() {
+      var c1 = overlay.getCorner(0);
+      var c2 = overlay.getCorner(1);
+      var c3 = overlay.getCorner(2);
+      var c4 = overlay.getCorner(3);
+
+      overlay.rotateBy(180);
+      expect([overlay.getCorner(2), overlay.getCorner(3)].members).to.equal([c1, c2].members);
+
+      overlay.rotateBy(180, 'deg');
+      expect([overlay.getCorner(2), overlay.getCorner(3)].members).to.equal([c3, c4].members);
+
+      overlay.rotateBy(Math.PI, 'rad');
+      expect([overlay.getCorner(2), overlay.getCorner(3)].members).to.equal([c1, c2].members);
+    });
+
+    it('Rotates the image counter clockwise for negative values', function() {
+      expect(overlay.getCorners().members).to.equal(overlay2.getCorners().members);
+
+      overlay.rotateBy(-45);
+      overlay2.getElement().style.transform += ' rotate(-45deg)';
+
+      expect(overlay.getCorners().members).to.equal(overlay2.getCorners().members);
+    });
+
+    it('Rotates the image by the remainder for values higher than the degrees in a circle', function() {
+      expect(overlay.getCorners().members).to.equal(overlay2.getCorners().members);
+      overlay.rotateBy(380);
+      overlay2.rotateBy(20);
+      expect(overlay.getCorners().members).to.equal(overlay2.getCorners().members);
+    });
+  });
+
+  describe('#getAngle', function() {
+    it('Returns the image\'s rotation angle in the specified units, otherwise in degrees', function() {
+      overlay.rotateBy(180);
+      expect(overlay.getAngle()).to.equal(180);
+      expect(overlay.getAngle('deg')).to.equal(180);
+      expect(overlay.getAngle('rad')).to.equal(3.14);
+    });
+
+    it('Returns the correct angle even when the image is flipped', function() {
+      var c1 = overlay.getCorner(0);
+      var c2 = overlay.getCorner(1);
+      var c3 = overlay.getCorner(2);
+      var c4 = overlay.getCorner(3);
+
+      expect(overlay.getAngle()).to.equal(0);
+      overlay.setCorners({ 0: c3, 1: c4, 2: c1, 3: c2 });
+      expect(overlay.getAngle()).to.equal(180);
+    });
+
+    it('Always returns a number greater than or equal to 0', function() {
+      expect(overlay.getAngle()).to.equal(0);
+      overlay.rotateBy(-45);
+      expect(overlay.getAngle()).to.equal(315);
+    });
+  });
+
+  describe('#setAngle', function() {
+    it('Sets the image\'s rotation angle in the specified units, otherwise in degrees', function() {
+      overlay.setAngle(45);
+      expect(overlay.getAngle('deg')).to.equal(45);
+
+      overlay.setAngle(90, 'deg');
+      expect(overlay.getAngle('deg')).to.equal(90);
+      expect(overlay.getAngle('rad')).to.equal(1.57);
+
+      overlay.setAngle(3.14, 'rad');
+      expect(overlay.getAngle('deg')).to.equal(180);
+      expect(overlay.getAngle('rad')).to.equal(3.14);
+    });
+
+    it('Sets the image\'s rotation angle to the remainder for values higher than the degrees in a circle', function() {
+      overlay.setAngle(380);
+      expect(overlay.getAngle('deg')).to.equal(20);
+    });
+
+    it('Subtracts negative values from the total degrees in a circle', function() {
+      overlay.setAngle(-340);
+      expect(overlay.getAngle('deg')).to.equal(20);
+    });
+
+    it('Sets the correct angle even when the image is flipped', function() {
+      var c1 = overlay.getCorner(0);
+      var c2 = overlay.getCorner(1);
+      var c3 = overlay.getCorner(2);
+      var c4 = overlay.getCorner(3);
+
+      expect(overlay.getAngle()).to.equal(0);
+      overlay.setCorners({ 0: c3, 1: c4, 2: c1, 3: c2 });
+      expect(overlay.getAngle()).to.equal(180);
+      overlay.setAngle(0);
+      expect(overlay.getAngle()).to.equal(0);
     });
   });
 });
