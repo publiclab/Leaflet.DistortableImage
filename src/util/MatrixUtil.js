955a49ea0063cd4d9257583bc766ba37e1703c0e
Akzharkyn Duisembiyeva <36645111+AkzharkynDM@users.noreply.github.com>
2020-04-14 19:44:07 +0200

    Renamed L.MatrixUtil functions to match ES6 (#607)
    * Renamed functions to match ES6
    
    * Added dist files
    
    * Added missing dist file
    
    * Dist
    
    * Commit dist
    
    Co-authored-by: sashadev-sky <sboginsky17@gmail.com>

--- src/util/MatrixUtil.js
+++ src/util/MatrixUtil.js
@@ -1,7 +1,7 @@
 L.MatrixUtil = {
 
   // Compute the adjugate of m
-  adj: function(m) {
+  adj(m) {
     return [
       m[4]*m[8]-m[5]*m[7], m[2]*m[7]-m[1]*m[8], m[1]*m[5]-m[2]*m[4],
       m[5]*m[6]-m[3]*m[8], m[0]*m[8]-m[2]*m[6], m[2]*m[3]-m[0]*m[5],
@@ -10,7 +10,7 @@ L.MatrixUtil = {
   },
 
   // multiply two 3*3 matrices
-  multmm: function(a, b) {
+  multmm(a, b) {
     var c = [];
     var i;
 
@@ -30,7 +30,7 @@ L.MatrixUtil = {
   },
 
   // multiply a 3*3 matrix and a 3-vector
-  multmv: function(m, v) {
+  multmv(m, v) {
     return [
       m[0]*v[0] + m[1]*v[1] + m[2]*v[2],
       m[3]*v[0] + m[4]*v[1] + m[5]*v[2],
@@ -39,7 +39,7 @@ L.MatrixUtil = {
   },
 
   // multiply a scalar and a 3*3 matrix
-  multsm: function(s, m) {
+  multsm(s, m) {
     var matrix = [];
 
     for (var i = 0, l = m.length; i < l; i++) {
@@ -49,7 +49,7 @@ L.MatrixUtil = {
     return matrix;
   },
 
-  basisToPoints: function(x1, y1, x2, y2, x3, y3, x4, y4) {
+  basisToPoints(x1, y1, x2, y2, x3, y3, x4, y4) {
     var m = [
       x1, x2, x3,
       y1, y2, y3,
@@ -64,22 +64,20 @@ L.MatrixUtil = {
     ]);
   },
 
-  project: function(m, x, y) {
+  project(m, x, y) {
     var v = L.MatrixUtil.multmv(m, [x, y, 1]);
 
     return [v[0]/v[2], v[1]/v[2]];
   },
 
-  general2DProjection: function(
+  general2DProjection(
       x1s, y1s, x1d, y1d,
       x2s, y2s, x2d, y2d,
       x3s, y3s, x3d, y3d,
       x4s, y4s, x4d, y4d
   ) {
-    var s = L.MatrixUtil
-        .basisToPoints(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s);
-    var d = L.MatrixUtil
-        .basisToPoints(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d);
+    var s = L.MatrixUtil.basisToPoints(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s);
+    var d = L.MatrixUtil.basisToPoints(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d);
     var m = L.MatrixUtil.multmm(d, L.MatrixUtil.adj(s));
 
     // Normalize to the unique matrix with m[8] == 1.
