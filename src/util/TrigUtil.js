3edaab053c07141cbf90ed5144923124e7f9ef12
Aquib Javed <aquib@shipwell.com>
2020-08-25 19:50:35 -0500

    update trigUtil method syntax to ES6

--- src/util/TrigUtil.js
+++ src/util/TrigUtil.js
@@ -1,16 +1,16 @@
 L.TrigUtil = {
 
-  calcAngle: function(x, y, unit = 'deg') {
+  calcAngle(x, y, unit = 'deg') {
     return unit === 'deg' ?
-        this.radiansToDegrees(Math.atan2(y, x)) :
-        Math.atan2(y, x);
+      this.radiansToDegrees(Math.atan2(y, x)) :
+      Math.atan2(y, x);
   },
 
-  radiansToDegrees: function(angle) {
+  radiansToDegrees(angle) {
     return (angle * 180) / Math.PI;
   },
 
-  degreesToRadians: function(angle) {
+  degreesToRadians(angle) {
     return (angle * Math.PI) / 180;
   },
 };
