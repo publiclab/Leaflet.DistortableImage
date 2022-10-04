44fdb20a6b7dbbf531f986a69c3ed5fb3eb2d59a
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2022-10-04 11:46:02 +0530

    Update DistortableImage.PopupBar.js

--- src/edit/toolbars/DistortableImage.PopupBar.js
+++ src/edit/toolbars/DistortableImage.PopupBar.js
@@ -8,34 +8,30 @@ L.DistortableImage.PopupBar = L.Toolbar2.Popup.extend({
     anchor: [0, -10],
   },
 
-  initialize: function(latlng, options) {
-    L.setOptions(this, options);
-    L.Toolbar2.Popup.prototype.initialize.call(this, latlng, options);
-  },
-
-  addHooks: function(map, ov) {
-    this.map = map;
-    this.ov = ov;
-  },
-
-  tools: function() {
-    if (this._ul) {
-      return this._ul.children;
-    }
-  },
-
-  clickTool: function(name) {
-    var tools = this.tools();
-    for (var i = 0; i < tools.length; i++) {
-      var tool = tools.item(i).children[0];
-      if (L.DomUtil.hasClass(tool, name)) {
-        tool.click();
-        return tool;
-      }
-    }
-    return false;
-  },
-});
+  -  initialize: function(latlng, options) {
++  initialize(latlng, options) {
+     L.setOptions(this, options);
+     L.Toolbar2.Popup.prototype.initialize.call(this, latlng, options);
+   },
+ 
+-  addHooks: function(map, ov) {
++  addHooks(map, ov) {
+     this.map = map;
+     this.ov = ov;
+   },
+ 
+-  tools: function() {
++  tools() {
+     if (this._ul) {
+       return this._ul.children;
+     }
+   },
+ 
+-  clickTool: function(name) {
++  clickTool(name) {
+     var tools = this.tools();
+     for (var i = 0; i < tools.length; i++) {
+       var tool = tools.item(i).children[0];
 
 L.distortableImage.popupBar = function(latlng, options) {
   return new L.DistortableImage.PopupBar(latlng, options);
