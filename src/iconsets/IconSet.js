59f8b47fe634b4f561c26ad36b43b9c1a61aa0c8
Khadeeejah <wuraola81@gmail.com>
2022-10-09 13:16:04 +0400

    fix: fixed error in the file

--- src/iconsets/IconSet.js
+++ src/iconsets/IconSet.js
@@ -1,17 +1,16 @@
 /* this is the baseclass other IconSets inherit from,
-* we don't use it directly */
+ * we don't use it directly */
 L.IconSet = L.Class.extend({
-
   _svg: '<svg xmlns="http://www.w3.org/2000/svg">',
 
   _symbols: '',
 
-  render: function() {
-    this.addSymbols(this._symbols);
-    return this._svg;
+  render() {
+    this.addSymbols(this._symbols)
+    return this._svg
   },
 
-  addSymbols: function(symbols) {
-    this._svg += symbols;
+  addSymbols(symbols) {
+    this._svg += symbols
   },
-});
+})
