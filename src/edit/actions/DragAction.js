c3fa6477ee6f750dd6e5ac0f8c4498a5a3976bb0
belfarz <115107231+belfarz@users.noreply.github.com>
2022-10-11 06:55:25 +0100

    Update DragAction.js

--- src/edit/actions/DragAction.js
+++ src/edit/actions/DragAction.js
@@ -13,7 +13,7 @@ L.DragAction = L.EditAction.extend({
   },
 
   addHooks: function() {
-    var edit = this._overlay.editing;
+    let edit = this._overlay.editing;
     edit._dragMode();
   },
 });
