03de1484373633172d9366ff2d6b3f41bea3a7ea
Ahmed Sahal <kabuur7@gmail.com>
2022-10-15 14:50:51 +0300

    Modified function getCmPerPixel to es6 funcion in src/util/ImageUtil.js

--- src/util/ImageUtil.js
+++ src/util/ImageUtil.js
@@ -1,12 +1,12 @@
 L.ImageUtil = {
 
-  getCmPerPixel(overlay) {
-    var map = overlay._map;
+  getCmPerPixel:((overlay)=> {
+    let map = overlay._map;
 
-    var dist = map
+    let dist = map
         .latLngToLayerPoint(overlay.getCorner(0))
         .distanceTo(map.latLngToLayerPoint(overlay.getCorner(1)));
 
     return (dist * 100) / overlay.getElement().width;
-  },
+  }),
 };
