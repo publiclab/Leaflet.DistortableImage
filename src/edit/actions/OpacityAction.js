311bf9a8818e7957e3292d33d587e4c857616f2a
themacboy <themacboy72@gmail.com>
2022-10-22 04:12:38 +0200

    posar icona opacities a la barra

--- src/edit/actions/OpacityAction.js
+++ src/edit/actions/OpacityAction.js
@@ -5,6 +5,8 @@ L.OpacityAction = L.EditAction.extend({
     let use;
     let tooltip;
 
+    use = 'opacity';
+    /*
     if (edit._transparent) {
       use = 'opacity_empty';
       tooltip = overlay.options.translation.makeImageOpaque;
@@ -12,6 +14,7 @@ L.OpacityAction = L.EditAction.extend({
       use = 'opacity';
       tooltip = overlay.options.translation.makeImageTransparent;
     }
+    */
 
     options = options || {};
     options.toolbarIcon = {
@@ -21,7 +24,7 @@ L.OpacityAction = L.EditAction.extend({
       className: mode === 'lock' ? 'disabled' : '',
     };
 
-    L.DistortableImage.action_map.o = mode === 'lock' ? '' : '_toggleOpacity';
+    //L.DistortableImage.action_map.o = mode === 'lock' ? '' : '_toggleOpacity';
 
     L.EditAction.prototype.initialize.call(this, map, overlay, options);
   },
@@ -32,6 +35,6 @@ L.OpacityAction = L.EditAction.extend({
 
     L.IconUtil.toggleXlink(link, 'opacity', 'opacity_empty');
     L.IconUtil.toggleTitle(link, 'Make Image Transparent', 'Make Image Opaque');
-    edit._toggleOpacity();
+    //edit._toggleOpacity();
   },
 });
