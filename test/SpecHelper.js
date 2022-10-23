20d918a3dbf3645c198af9bc0a877768338c6c3e
Lili Yao <103026397+liliyao2022@users.noreply.github.com>
2022-10-23 12:22:52 +1100

    Update SpecHelper.js
    Updated es5 to es6, replaced var to let.

--- test/SpecHelper.js
+++ test/SpecHelper.js
@@ -17,7 +17,7 @@ chai.simulateEvent = function simulateEventFn(el, type, params) {
     bubbles: type != 'mouseleave' && type != 'mouseeenter',
     cancelable: type != 'mousemove' && type != 'mouseleave' && type != 'mouseeenter',
   };
-  var e = new MouseEvent(type, params);
+  let e = new MouseEvent(type, params);
   return el.dispatchEvent(e);
 };
 
@@ -30,17 +30,17 @@ chai.simulateEvent = function simulateEventFn(el, type, params) {
  *     > true
  */
 chai.use(function(chai, utils) {
-  var Assertion = chai.Assertion;
+  let Assertion = chai.Assertion;
   Assertion.addMethod('closeToLatLng', function(actual, delta, message) {
-    var obj = utils.flag(this, 'object');
+    let obj = utils.flag(this, 'object');
 
     delta = delta || 1e-4;
 
     expect(obj).to.have.property('lat');
     expect(obj).to.have.property('lng');
 
-    var lat = new Assertion(obj.lat);
-    var lng = new Assertion(obj.lng);
+    let lat = new Assertion(obj.lat);
+    let lng = new Assertion(obj.lng);
 
     utils.transferFlags(this, lat, false);
     utils.transferFlags(this, lng, false);
