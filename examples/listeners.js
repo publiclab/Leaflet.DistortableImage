80da8516492989a380122dcf1baeadc683ef4a56
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2022-11-20 00:57:37 +0530

    created listeners.js
    transfere the content from listeners.html to new file listeners.js

---
+++ examples/listeners.js
@@ -0,0 +1,17 @@
+<script src="./js/listeners.js" defer></script>
+var map;
+     (function(){
+       map = L.map('map').setView([51.505, -0.09], 13);
+       map.addGoogleMutant();
+       map.whenReady(function() {
+         img = L.distortableImageOverlay('example.jpg', {
+           selected: true,
+         }).addTo(map);
+
+         // Wait until image is loaded before setting up DOM element listeners
+         L.DomEvent.on(img.getElement(), 'load', function() {
+           L.DomEvent.on(img, 'edit', function() { alert('edited'); });
+           L.DomEvent.on(img.getElement(), 'mouseup touchend', function() { alert('edited'); });
+         });
+       });
+     })();
